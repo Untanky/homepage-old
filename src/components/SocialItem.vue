c591210fba53b036aac2b2e9b4ba102b127475a8
Untanky <mail@lukasgrimm.com>
2020-03-17 22:13:24 +0100

    Added socials list

---
+++ src/components/SocialItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="social-item">
+    <img
+      class="social-logo"
+      :src="social.imageURI"
+      :alt="network"
+    >
+    <div class="wrapper">
+      <h3 class="network-name">
+        {{ social.network }}
+      </h3>
+      <div class="handle">
+        <a :href="social.link">{{ social.handle }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialItem',
+  props: {
+    social: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../_variables";
+$logo-size: 96px;
+
+.social-item {
+  max-width: 360px;
+  height: $logo-size;
+  display: grid;
+  grid-template-columns: $logo-size 1fr;
+  gap: 2em;
+  background-color: white;
+  border-left: $border-primary;
+  border-radius: $border-radius;
+  margin: 0 auto;
+  box-shadow: $shadow;
+
+  .social-logo {
+    box-sizing: border-box;
+    height: $logo-size;
+    padding: 1em;
+    border-top: $border-primary;
+    border-bottom: $border-primary;
+    border-radius: $border-radius 0 0 $border-radius;
+  }
+
+  .wrapper {
+    text-align: left;
+  }
+}
+</style>
--- src/views/Socials.vue
+++ src/views/Socials.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Socials</h1>
+  <div class="socials">
+    <h1>Socials</h1>
+    <SocialList />
+  </div>
 </template>
+
+<script>
+import SocialList from '../components/SocialList.vue';
+
+export default {
+  name: 'Socials',
+  components: { SocialList },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
