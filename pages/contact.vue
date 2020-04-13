8a48c04080b9baf7b5ec8a184433295931683de7
Lukas Grimm <lukaskingsmail@gmail.com>
2020-04-13 13:17:46 +0200

    Scoped the css code in page components

--- pages/contact.vue
+++ pages/contact.vue
@@ -11,7 +11,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .title {
   text-align: center;
   font-weight: 800;
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <template>
-  <h1 class="title">
-    Home
-  </h1>
+  <div class="home">
+    <introduction />
+    <website-content
+      heading="Portfolio"
+    >
+      Interested in Lukas? You can check if he matches your requirements for an open position
+      by reading through his portfolio. You can check his skills and previous employments there.
+    </website-content>
+  </div>
 </template>
 
 <script>
+import Introduction from '../components/Introduction.vue';
+import WebsiteContent from '../components/WebsiteContent.vue';
 
 export default {
-
+  components: {
+    Introduction,
+    WebsiteContent,
+  },
 };
 </script>
 
-<style>
+<style scoped>
 .title {
-  text-align: center;
   font-weight: 800;
 }
 </style>
