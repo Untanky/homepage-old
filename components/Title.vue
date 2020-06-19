4057aeea826ad80b2c711c09735d0f3c30071f72
Lukas Grimm <lukaskingsmail@gmail.com>
2020-06-19 19:40:29 +0200

    Last changes

--- components/Title.vue
+++ components/Title.vue
@@ -11,11 +11,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/scss/responsive';
+
 h1.title {
   text-align: center;
   font-size: 48px;
   font-weight: 800;
   padding: 0;
-  margin: 0em 0 2em 0;
+  margin: 2em 0 2em 0;
+
+  @include tablet-up {
+    margin-top: 0;
+  }
 }
 </style>
