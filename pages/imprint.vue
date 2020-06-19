c8d492bc6aa25c9b97b80975944bb1f641e25371
Lukas Grimm <lukaskingsmail@gmail.com>
2020-06-19 18:21:34 +0200

    Finished i18n

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,15 +1,38 @@
+<i18n>
+{
+  "en": {
+    "tmg": "Information in accordance with Section 5 TMG",
+    "represented": "Represented by",
+    "contactInformation": "Contact Information",
+    "telephone": "Telephone",
+    "email": "E-Mail"
+  },
+  "de": {
+    "tmg": "Angaben gemäß § 5 TMG",
+    "represented": "Vertreten durch",
+    "contactInformation": "Kontaktdaten",
+    "telephone": "Telefon",
+    "email": "E-Mail"
+  }
+}
+</i18n>
+
 <template>
   <main class="imprint">
-    <h1>Imprint</h1>
-    Information in accordance with Section 5 TMG<br><br>
+    <h1>{{ $t('pages.imprint') }}</h1>
+    {{ $t('tmg') }}<br><br>
 
-    Ahornallee 16 <br>
-    12555 Berlin <br>
-    Germany <br>
+    Lukas Grimm <br>
+    Britzer Damm 41 <br>
+    12347 Berlin <br>
+    Germany <br><br>
 
-    <h2>Contact Information</h2>
-    Telephone: +49 1512 910 2880<br>
-    E-Mail: <a href="mailto:lukaskingsmail@gmail.com">lukaskingsmail@gmail.com</a><br>
+    <b>{{ $t('represented') }}</b><br>
+    Lukas Grimm
+
+    <h2>{{ $t('contactInformation') }}</h2>
+    {{$t('telephone')}}: +49 1512 910 2880<br>
+    {{$t('email')}}: <a href="mailto:lukaskingsmail@gmail.com">lukaskingsmail@gmail.com</a><br>
 
   </main>
 </template>
