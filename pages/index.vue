3349e82f27ece5545dd3cb2e91119b15063ae7f1
Lukas Grimm <lukaskingsmail@gmail.com>
2020-11-11 22:39:40 +0100

    Project setup

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  p {{text}}
+</template>
+
+<script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Vue from 'vue';
+
+export default Vue.extend({
+  data: () => ({
+    text: 'Hello World',
+  }),
+});
+</script>
