51c79d7d90217c34404bb891b0cc0e3e36526101
Lukas Grimm <lukaskingsmail@gmail.com>
2020-11-30 15:25:15 +0100

    Added missing GridLayout components

---
+++ components/layouts/GridLayout.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  section.content.w-full.relative.my-16
+    .container.mx-auto.grid.grid-cols-1.gap-4.auto-rows-80.grid-flow-column-dense(class="lg:grid-cols-3")
+      .card.m-0 Hello
+      .card.m-0 Hello
+      .card.m-0 Hello
+      .card.m-0.row-span-3 Hello
+      .card.m-0 Hello
+      .card.m-0.row-span-2 Hello
+      .card.m-0.row-span-2 Hello
+</template>
