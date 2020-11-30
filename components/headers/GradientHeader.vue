4ccbedf4aaf2e73bd37b88e0d6b485cbbc8a769a
Lukas Grimm <lukaskingsmail@gmail.com>
2020-11-30 17:12:35 +0100

    Made Title not selectable

--- components/headers/GradientHeader.vue
+++ components/headers/GradientHeader.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   header.h-full.w-full.relative
     .bg-gradient-to-r.from-primary-500.to-secondary-500.w-full.block.flex.justify-center.items-center.mb-6(class="h-2/3 md:h-4/5 lg:h-5/6")
-      h1.text-6xl.font-bold.whitespace-normal.text-center(class="md:text-7xl lg:text-9xl xl:text-10xl") Lukas Grimm
+      h1.text-6xl.font-bold.whitespace-normal.text-center.select-none(class="md:text-7xl lg:text-9xl xl:text-10xl") Lukas Grimm
     .text-center
       a.block.link.text-2xl.mb-4(class="md:mr-12 md:inline-block after:chevron" href="/contact") {{ $t('titlePage.contactCTA') }}
       a.block.link.text-2xl.mb-4(class="md:inline-block after:chevron" href="/contact") {{ $t('titlePage.downloadCTA') }}
