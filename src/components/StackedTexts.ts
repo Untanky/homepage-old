5cb5f0a5832263a7ef1a314804b3d51236a2f9a2
Lukas Grimm <lukaskingsmail@gmail.com>
2020-12-23 17:53:57 +0100

    Removed StackedText Component

--- src/components/StackedTexts.ts
+++
@@ -1,9 +0,0 @@
-import { FormattedText } from '.';
-import ComponentData from '../interfaces/ComponentData';
-
-interface StackedTexts extends ComponentData {
-  formattedTexts: FormattedText[];
-  classes: string[]
-}
-
-export default StackedTexts;
