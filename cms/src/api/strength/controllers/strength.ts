c84423e5b338bb8eeb0d955e29a6059798fa1988
Lukas Grimm <lukaskingsmail@gmail.com>
2023-02-17 18:19:12 +0100

    Setup content types

---
+++ cms/src/api/strength/controllers/strength.ts
@@ -0,0 +1,7 @@
+/**
+ * strength controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::strength.strength');
---
+++ cms/src/api/strength/services/strength.ts
@@ -0,0 +1,7 @@
+/**
+ * strength service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::strength.strength');
